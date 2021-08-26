b1a43a9b4e51b8e6928ff58c1bccd363d11fda30
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-08-25 21:16:18 -0400

    feat(theme): add fonts

--- gatsby-theme-mrv1k/tailwind.config.js
+++ gatsby-theme-mrv1k/tailwind.config.js
@@ -1,9 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: [],
   // purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+        // font family is sans without a class
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+        mono: ["'JetBrains Mono'", ...defaultTheme.fontFamily.mono],
+      },
       colors: {
         // based on my favicon
         logo: {
