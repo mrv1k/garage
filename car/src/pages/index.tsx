46cbf9823ca8eea9cca4b3ce0ca3bec7d9fcf7f9
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-08-30 20:36:57 -0400

    fixes

--- car/src/pages/index.tsx
+++ car/src/pages/index.tsx
@@ -7,34 +7,36 @@ import Seo from "../components/Seo";
 const IndexPage = (): JSX.Element => (
   <Layout>
     <Seo title="Home" />
-    <h1 className="">Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p className="font-sans 80-digits">
-      1234567890123456789012345678901234567890123456789012345678901234567890123456789!
-    </p>
-    <p className="font-sans 80-chars">
-      abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
-    </p>
-    <br />
-    <p className="font-serif 80-digits">
-      1234567890123456789012345678901234567890123456789012345678901234567890123456789!
-    </p>
-    <p className="font-serif 80-chars">
-      abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
-    </p>
-    <br />
-    <p className="font-mono 80-digits">
-      1234567890123456789012345678901234567890123456789012345678901234567890123456789!
-    </p>
-    <p className="font-mono 80-chars">
-      abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
-    </p>
+    <div className="col-span-8">
+      <h1 className="">Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <p className="font-sans 80-digits">
+        1234567890123456789012345678901234567890123456789012345678901234567890123456789!
+      </p>
+      <p className="font-sans 80-chars">
+        abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
+      </p>
+      <br />
+      <p className="font-serif 80-digits">
+        1234567890123456789012345678901234567890123456789012345678901234567890123456789!
+      </p>
+      <p className="font-serif 80-chars">
+        abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
+      </p>
+      <br />
+      <p className="font-mono 80-digits">
+        1234567890123456789012345678901234567890123456789012345678901234567890123456789!
+      </p>
+      <p className="font-mono 80-chars">
+        abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
+      </p>
 
-    <br />
-    <p>
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+      <br />
+      <p>
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </div>
   </Layout>
 );
 
