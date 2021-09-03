4b68305b068275d71fae990daa7a587a60fce750
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-02 21:38:29 -0400

    blog config

--- car/gatsby-config.js
+++ car/gatsby-config.js
@@ -45,14 +45,7 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-purgecss`,
-      options: {
-        // printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
+      options: { tailwind: true },
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
@@ -87,6 +80,45 @@ module.exports = {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
         siteUrl: `https://mrv1k.me`,
+      },
+    },
+
+    // blog plugins
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/../boxes/blog`,
+        name: "blog",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/../boxes/assets`,
+        name: "assets",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1024,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
       },
     },
   ],
