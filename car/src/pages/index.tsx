32c762f421b8d6fc636a5a23b98b07e689e1c763
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-05 22:51:09 -0400

    wip: clean index page

--- car/src/pages/index.tsx
+++ car/src/pages/index.tsx
@@ -1,42 +1,14 @@
 import * as React from "react";
-import { Link } from "gatsby";
 
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
 const IndexPage = (): JSX.Element => (
-  <Layout>
-    <Seo title="Home" />
-    <div className="col-core">
-      <h1 className="">Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <p className="font-sans 80-digits">
-        1234567890123456789012345678901234567890123456789012345678901234567890123456789!
-      </p>
-      <p className="font-sans 80-chars">
-        abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
-      </p>
-      <br />
-      <p className="font-serif 80-digits">
-        1234567890123456789012345678901234567890123456789012345678901234567890123456789!
-      </p>
-      <p className="font-serif 80-chars">
-        abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
-      </p>
-      <br />
-      <p className="font-mono 80-digits">
-        1234567890123456789012345678901234567890123456789012345678901234567890123456789!
-      </p>
-      <p className="font-mono 80-chars">
-        abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzkk
-      </p>
-
-      <br />
-      <p>
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-      </p>
-    </div>
+  <Layout title="Viktor Khotimchenko">
+    <>
+      <Seo title="Viktor Khotimchenko" />
+      <p>#wip</p>
+    </>
   </Layout>
 );
 
