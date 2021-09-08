6560ac40197cbd72de4f2c87da1f9779f98dab9f
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-08 16:51:53 -0400

    chore(deps): remove fontsource, remark-prism

--- car/gatsby-browser.js
+++ car/gatsby-browser.js
@@ -4,14 +4,4 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-// FONTS
-// https://fontsource.org/docs/variable-fonts
-// Begin by importing both the variable and fallback font for non-compatible browsers.
-// import "@fontsource/inter/400.css"; // Weight 400.
-// import "@fontsource/inter/variable.css"; // Contains ONLY variable weights and no other axes.
-// import "@fontsource/jetbrains-mono/400.css"; // has variable version, but code snippets are always 400
-
-// import "prismjs/themes/prism-solarizedlight.css";
-import "prism-themes/themes/prism-gruvbox-dark.css";
-
 import "./theme.css";
--- car/tailwind.config.js
+++ car/tailwind.config.js
@@ -12,10 +12,6 @@ module.exports = {
         // font-family: ui-serif, Georgia, Cambria, "Times New Roman", Times, serif;
         // font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
         // font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
-        // ex-custom fonts
-        // serif: defaultTheme.fontFamily.serif,
-        // sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
-        // mono: ["'JetBrains Mono'", ...defaultTheme.fontFamily.mono],
       },
       colors: {
         // based on my favicon
