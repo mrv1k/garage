b506141ea6df572efa3f39843519db25e79071ed
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-10 17:51:23 -0400

    feat: Nav component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,25 +1,29 @@
-// import { Link } from "gatsby";
-import * as React from "react";
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
 
-const Navigation = (): JSX.Element => (
-  <nav className="row-start-1 mb-4 nav md:row-start-2 md:mb-0">
-    <ol className="flex md:flex-col">
-      <li>
-        {/* <Link activeClassName="active" to="/"> */}
-        home
-        {/* </Link> */}
-      </li>
-      <li>
-        {/* <Link activeClassName="active" to="/about"> */}
-        about
-        {/* </Link> */}
-      </li>
-      <li>
-        {/* <Link activeClassName="active" to="/blog"> */}
-        blog
-        {/* </Link> */}
-      </li>
-    </ol>
-  </nav>
-);
+const paths = ["/", "/about", "/blog"];
+
+const Navigation = (): JSX.Element => {
+  const router = useRouter();
+
+  return (
+    <nav className="row-start-1 mb-4 nav md:row-start-2 md:mb-0">
+      <ol className="flex md:flex-col">
+        {paths.map((path) => (
+          <li key={path}>
+            <Link href={path}>
+              <a className={router.pathname === path ? "active" : ""}>
+                {path.replace("/", (match, offset, string) => {
+                  if (string === "/") return "home";
+                  return "";
+                })}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+};
+
 export default Navigation;
