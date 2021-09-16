13a89385d1f72cc9d865b63788cb950b56da5f9a
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-16 17:33:08 -0400

    wip: add work in progress banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,13 @@ import Layout from "../components/Layout";
 
 const IndexPage = (): JSX.Element => (
   <Layout title="Viktor Khotimchenko">
-    <p>#wip</p>
+    <div
+      className="p-4 text-yellow-700 bg-yellow-100 border-l-4 border-yellow-500 v-layout-core"
+      role="alert"
+    >
+      <p className="font-bold">Caution!</p>
+      <p>🚧👷‍♂️ Hard hats required. Website is under active construction ⚒️🏚</p>
+    </div>
   </Layout>
 );
 export default IndexPage;
