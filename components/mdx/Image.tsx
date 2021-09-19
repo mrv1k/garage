8554910b89e9892609276c4d5c6e784b295fb3dc
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-18 21:45:37 -0400

    wip: next/image works with src prop!

--- components/mdx/Image.tsx
+++ components/mdx/Image.tsx
@@ -1,22 +1,20 @@
+// ! Images are not as optimized on Gatsby
 import Image from "next/image";
 
-// ! Images are not as optimized on Gatsby
-const MDXImage = ({ src, alt: altWithSize }: { src: string; alt: string }) => {
-  const [alt, size] = altWithSize.split(",");
-  const [width, height = "512"] = size.split("x");
-
-  // ! remote images are not handled yet ![Alt Text](https://get.svg.workers.dev/?s=64&f=gray "Image Title")
-  // TODO: add support for remote images
+const MDXImage = ({ src }) => {
+  // return <div>wtf</div>;
+  // return <Image src={src} layout="fill" />;
   return (
     <Image
-      alt={alt}
+      // alt={alt}
       src={src}
       // intrinsic is default, alternative would be to use responsive
       layout="intrinsic"
-      width={width}
-      height={height}
+      width={512}
+      height={512}
     />
   );
 };
+export { MDXImage };
 
 export default MDXImage;
