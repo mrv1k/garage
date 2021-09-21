92fb013eb28d9b024c4ab0450ec22d9cceb5b885
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-20 21:23:09 -0400

    feat: add Grid dev validation

--- components/mdx/Grid.tsx
+++ components/mdx/Grid.tsx
@@ -1,11 +1,38 @@
 import { PropsWithChildren } from "react";
 
-export const Grid = ({
-  children,
-  className = "v-layout-core",
-}: PropsWithChildren<{ className: string }>) => {
-  if (className && className.includes("span"))
-    console.warn("*-span is floats around the page, it's better to explicit");
+const tailwindBreakpoints = ["sm", "md", "lg", "xl", "2xl"];
+
+type Props = PropsWithChildren<{ className: string }>;
+
+//  Validation came after spending hours trying to figure out why grid
+// had multiple columns instead of 1
+// Setting grid-col-1 on the grid also doesn't work
+
+export const Grid = ({ children, className = "v-layout-core" }: Props) => {
+  if (process.env.NODE_ENV === "development") {
+    let error = false;
+
+    if (className && !className.includes("v-layout")) {
+      if (className.includes("span-start") || className.includes("span-end")) {
+        error = true;
+        console.error("*-span floats around, be explicit or use v-layout-*");
+      }
+      if (tailwindBreakpoints.some((bp) => className.includes(bp)) === false) {
+        console.log(
+          className,
+          !tailwindBreakpoints.some((bp) => className.includes(bp))
+        );
+        error = true;
+        console.error("MDX requires responsive modifiers or it breaks");
+      }
+    }
+    return (
+      <div className={className}>
+        {error && "Broken grid below!"}
+        {children}
+      </div>
+    );
+  }
 
   return <div className={className}>{children}</div>;
 };
