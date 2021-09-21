38f6b2bfef84469a3d8ce12c08eb55a4afb4de43
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-21 14:15:26 -0400

    delete remark-mdx-images
    this plugin set me on trajectory that I ended working with mdx pages

--- lib/mdx-bundler.ts
+++ lib/mdx-bundler.ts
@@ -1,14 +1,10 @@
-import { BuildOptions } from "esbuild";
 import { bundleMDXFile } from "mdx-bundler";
 import { BundleMDXOptions } from "mdx-bundler/dist/types";
 import path from "path";
 import process from "process";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
-import { remarkMdxImages } from "remark-mdx-images";
 import remarkFootnotes from "remark-footnotes";
-
-import { SUPPORTED_IMAGE_EXTENSIONS } from "./constants";
 
 // ! Can't use named exports from this file. Well, can export, can't import.
 // Module not found: Can't resolve 'builtin-modules'
@@ -33,20 +29,11 @@ if (process.platform === "win32") {
   );
 }
 
-// 80% flex, 20% usefulness. One source for image extensions
-// Result without flex: { ".png": "file", ".jpg": "file", ".jpeg": "file", ".gif": "file", }
-const imageExtensions: BuildOptions["loader"] =
-  SUPPORTED_IMAGE_EXTENSIONS.reduce(
-    (acc, extension) => ({ ...acc, [extension]: "file" }),
-    {}
-  );
-
 const bundleMDXFileWithOptions = async (mdxPostDir: string) => {
   const xdmOptions: BundleMDXOptions["xdmOptions"] = (options) => {
     options.remarkPlugins = [
       ...(options.remarkPlugins ?? []),
       remarkGfm,
-      remarkMdxImages,
       remarkFootnotes,
     ];
     options.rehypePlugins = [...(options.rehypePlugins ?? []), rehypeHighlight];
@@ -59,19 +46,6 @@ const bundleMDXFileWithOptions = async (mdxPostDir: string) => {
   return await bundleMDXFile(mdxIndex, {
     cwd: mdxPostDir,
     xdmOptions,
-    esbuildOptions: (options) => {
-      options.outdir = path.join(process.cwd(), "public", "imag_es");
-      options.loader = {
-        ...options.loader,
-        ...imageExtensions,
-      };
-
-      options.publicPath = "/imag_es";
-      // Set write to true so that esbuild will output the files.
-      options.write = true;
-
-      return options;
-    },
   });
 };
 
