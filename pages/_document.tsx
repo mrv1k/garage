827370f1d34a3ffb6b9f6b203e0ca916056ab32f
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-23 22:00:55 -0400

    customize document, inline background color

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  // static async getInitialProps(ctx: DocumentContext) {
+  //   const initialProps = await Document.getInitialProps(ctx);
+  //   return initialProps;
+  // }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body
+          style={{
+            backgroundColor: "#fcf8ed",
+          }}
+        >
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
