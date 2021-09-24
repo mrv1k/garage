647b0a7b33258959bf7d7f89c93c90c397b80cf9
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-23 21:44:46 -0400

    feat: goto top/bottom buttons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,20 @@ import "../styles/globals.css";
 
 function Garage({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <div className="garage">
+    <div className="relative garage">
+      <div id="top" className="absolute top-0 right-0 pt-5 pr-5">
+        <a href="#bottom" className="goto-button">
+          goto bottom
+        </a>
+      </div>
+
       <Component {...pageProps} />
+
+      <div id="bottom" className="absolute bottom-0 right-0 pb-5 pr-5">
+        <a href="#top" className="goto-button">
+          goto top
+        </a>
+      </div>
     </div>
   );
 }
