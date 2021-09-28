85b3deefd130228891b7cd67851c570184f7b1c1
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-28 12:30:05 -0400

    style: always display button on new line

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,14 +21,16 @@ const IndexPage = (): JSX.Element => (
     </div>
 
     <div className="v-layout-core">
-      I’m a developer specializing in building components that last. Currently,
-      building a website for a client called myself. Open to new opportunities.
-      <a
-        className="inline-block py-3 mt-8 !no-underline border rounded px-11 border-logo-orange hover:bg-logo-orange hover:text-logo-beige"
-        href="mailto:viktorkhotimchenko@gmail.com"
-      >
-        Get in touch
-      </a>
+      Currently, building this website to be my public learning platform. Open
+      to new opportunities.
+      <div className="mt-8">
+        <a
+          className="!no-underline border rounded py-3 px-11 border-logo-orange hover:bg-logo-orange hover:text-logo-beige"
+          href="mailto:viktorkhotimchenko@gmail.com"
+        >
+          Get in touch
+        </a>
+      </div>
       {/* <button className="p-4 rounded bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
           Sign up
         </button> */}
