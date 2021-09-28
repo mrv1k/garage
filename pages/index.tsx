d669388e48686067b8f520215f6237f1fa008c5c
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-28 18:40:25 -0400

    wip: 100vh and flex

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,33 +7,34 @@ const IndexPage = (): JSX.Element => (
       <title>Viktor Khotimchenko </title>
     </Head>
 
-    <div className="min-h-screen v-layout-full">
-      <span className="text-xl">Hi, I’m</span>
+    <div className="min-h-screen v-layout-full v-home-flex">
       <header>
+        <span className="text-xl">Hi, my name is</span>
         <h1 className="text-5xl font-bold">
           <span className="text-v-orange">Viktor Khotimchenko</span>.
         </h1>
       </header>
 
-      <div className="col-span-9 pt-3 pb-6 text-5xl font-bold">
-        I bring your designs to life with React and TypeScript
+      <div className="col-span-9 pt-3 pb-6 text-5xl font-bold basis-75 ">
+        I bring designs to life with React and TypeScript
       </div>
 
-      <div className="v-layout-core">
-        Currently, building this website to be my public learning platform. Open
-        to new opportunities.
-        <div className="mt-8">
-          <a
-            className="!no-underline border rounded py-3 px-11 border-v-orange hover:bg-v-orange hover:text-v-beige"
-            href="mailto:viktorkhotimchenko@gmail.com"
-          >
-            Get in touch
-          </a>
-        </div>
-        {/* <button className="p-4 rounded bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
+      <p>
+        Frontend Developer. Currently, building this website to serve as my
+        self-education log. Open to new opportunities.
+      </p>
+
+      <div className="mt-8">
+        <a
+          className="!no-underline border rounded py-3 px-11 text-v-orange border-v-orange hover:bg-v-orange hover:text-v-beige"
+          href="mailto:viktorkhotimchenko@gmail.com"
+        >
+          Get in touch
+        </a>
+      </div>
+      {/* <button className="p-4 rounded bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
           Sign up
         </button> */}
-      </div>
     </div>
 
     <div className="min-h-screen v-layout-full">page 2</div>
