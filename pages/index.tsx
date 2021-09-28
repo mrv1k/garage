2a10fe261d5a51f39f9bc4e2cf869383754fcb1a
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-09-28 12:45:27 -0400

    wip: min-h-screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,34 +7,36 @@ const IndexPage = (): JSX.Element => (
       <title>Viktor Khotimchenko </title>
     </Head>
 
-    <div className="v-layout-core">
+    <div className="min-h-screen v-layout-full">
       <span className="text-xl">Hi, I’m</span>
       <header>
         <h1 className="text-5xl font-bold">
           <span className="text-v-orange">Viktor Khotimchenko</span>.
         </h1>
       </header>
-    </div>
 
-    <div className="col-span-9 pt-3 pb-6 text-5xl font-bold">
-      I bring your designs to life with React and TypeScript
-    </div>
-
-    <div className="v-layout-core">
-      Currently, building this website to be my public learning platform. Open
-      to new opportunities.
-      <div className="mt-8">
-        <a
-          className="!no-underline border rounded py-3 px-11 border-v-orange hover:bg-v-orange hover:text-v-beige"
-          href="mailto:viktorkhotimchenko@gmail.com"
-        >
-          Get in touch
-        </a>
+      <div className="col-span-9 pt-3 pb-6 text-5xl font-bold">
+        I bring your designs to life with React and TypeScript
       </div>
-      {/* <button className="p-4 rounded bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
+
+      <div className="v-layout-core">
+        Currently, building this website to be my public learning platform. Open
+        to new opportunities.
+        <div className="mt-8">
+          <a
+            className="!no-underline border rounded py-3 px-11 border-v-orange hover:bg-v-orange hover:text-v-beige"
+            href="mailto:viktorkhotimchenko@gmail.com"
+          >
+            Get in touch
+          </a>
+        </div>
+        {/* <button className="p-4 rounded bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">
           Sign up
         </button> */}
+      </div>
     </div>
+
+    <div className="min-h-screen v-layout-full">page 2</div>
   </Layout>
 );
 export default IndexPage;
